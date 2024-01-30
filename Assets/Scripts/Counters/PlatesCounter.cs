e095b5e4ec8fbcdd502fef13b4be19929930b96b
Aqil TrueXR <aqil.irwan@truexr.com.my>
2024-01-30 20:48:09 +0800

    Initial commit

---
+++ Assets/Scripts/Counters/PlatesCounter.cs
@@ -0,0 +1,47 @@
+using System;
+using UnityEngine;
+
+public class PlatesCounter : BaseCounter
+{
+    public event EventHandler OnPlatesSpawned;
+    public event EventHandler OnPlatesRemoved;
+
+    [SerializeField] private KitchenObjectSO plateKitchenObjectSO;
+
+    private float spawnPlateTimer;
+    private float spawnPlateTimerMax = 4f;
+    private int platesSpawnedAmount;
+    private int platesSpawnedAmountMax = 4;
+
+    private void Update()
+    {
+        spawnPlateTimer += Time.deltaTime;
+
+        if(spawnPlateTimer > spawnPlateTimerMax)
+        {
+            spawnPlateTimer = 0f;
+
+            if(KitchenGameManager.Instance.IsGamePlaying() && platesSpawnedAmount < platesSpawnedAmountMax)
+            {
+                platesSpawnedAmount++;
+
+                OnPlatesSpawned? .Invoke(this, EventArgs.Empty);
+            }
+        }  
+    }
+
+    public override void Interact(Player player)
+    {
+        if(!player.HasKitchenObject())
+        {
+            if(platesSpawnedAmount > 0)
+            {
+                platesSpawnedAmount--;
+
+                KitchenObject.SpawnKitchenObject(plateKitchenObjectSO, player);
+
+                OnPlatesRemoved? .Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+}
